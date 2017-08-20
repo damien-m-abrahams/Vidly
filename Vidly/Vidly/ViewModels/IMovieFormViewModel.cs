7310b87191776b14bab02e08bb148f5f82aeb0cb
damienabrahams <damien.abrahams@insightsoftware.com>
2017-08-20 10:53:10 +0800

    New Movie form

---
+++ Vidly/Vidly/ViewModels/IMovieFormViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+	public interface IMovieFormViewModel : IViewModel
+	{
+		IEnumerable<GenreType> GenreTypes { get; set; }
+
+		Movie Movie { get; set; }
+	}
+}
---
+++ Vidly/Vidly/ViewModels/MovieFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+	public class MovieFormViewModel : IMovieFormViewModel
+	{
+		public INavigationViewModel Navigation { get; set; }
+
+		public IEnumerable<GenreType> GenreTypes { get; set; }
+
+		public Movie Movie { get; set; }
+	}
+}
