a6e1870111395c60a2799ae016c50c4f61f0df0a
damienabrahams <damien.abrahams@insightsoftware.com>
2017-08-21 14:33:06 +0800

    JSON serialization settings

--- Vidly/Vidly/App_Start/WebApiConfig.cs
+++ Vidly/Vidly/App_Start/WebApiConfig.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web.Http;
 using System.Web.Mvc;
 using Microsoft.Practices.Unity;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Unity.Mvc5;
 using Vidly.Models;
 
@@ -13,6 +15,10 @@ namespace Vidly
     {
         public static void Register(HttpConfiguration config)
         {
+	        var settings = config.Formatters.JsonFormatter.SerializerSettings;
+			settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+			settings.Formatting = Formatting.Indented;
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
